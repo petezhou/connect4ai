0e861ba5048556c66e765f51ce1593cd8965f0c7
Peter <p29zhou@edu.uwaterloo.ca>
2019-12-25 13:10:56 -0500

    initial commit

---
+++ ai/static/react/precache-manifest.4c93b3190fdc6a393530e539879446be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61ea0d6863f4dfd04db22dbc8581c23",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "9eb94f9a60b73c06e12d",
+    "url": "/static/react/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/static/react/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/react/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9eb94f9a60b73c06e12d",
+    "url": "/static/react/js/main.6accd2c5.chunk.js"
+  },
+  {
+    "revision": "21d74455c143b18cdc73",
+    "url": "/static/react/js/runtime-main.c8d8d2c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
